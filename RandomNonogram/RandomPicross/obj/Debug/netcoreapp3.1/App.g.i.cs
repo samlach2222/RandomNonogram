92408cd970588c09f26a4b4b176e02bff7511e11
samlach2222 <lachaudsamuel@protonmail.com>
2021-12-22 22:34:18 +0100

    Add files for readme

--- RandomNonogram/RandomPicross/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ RandomNonogram/RandomPicross/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -59,7 +59,7 @@ namespace Logic {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/HMI;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/HMI;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
